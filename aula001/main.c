7259e8e1f58360c9313f6cb24411422011b24219
mauriciopbraga <mauriciobraga100@gmail.com>
2020-04-16 01:29:45 -0300

    aulas 1-9

---
+++ aula001/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h> /*Declara  os caracteres especiais*/
+
+int main() {
+
+    setlocale(LC_ALL, "portuguese"); /*Seta o idioma*/
+
+    printf("Olá, mundo.\nMeu nome é Mauricio!\n"); /*Imprime o texto na tela*/
+
+    system("pause"); /*Pausa o texto na tela*/
+    return 0; /*Retorna ao início*/
+}
