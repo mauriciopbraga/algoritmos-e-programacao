33102ab9740c28ec0e2d371517ff276d124966f7
mauriciopbraga <mauriciobraga100@gmail.com>
2020-05-14 19:10:51 -0300

    aula 15

--- aula-16/exercicio1/exercicio1.c
+++ aula-16/exercicio1/exercicio1.c
@@ -1,3 +1,5 @@
+//Escrever um algoritmo capaz de ler as letras/teclas do teclado e identificar se a letra/tecla é ou não uma vogal.
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <locale.h>
---
+++ aula-16/exercicio2/exercicio2.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+
+    setlocale(LC_ALL, "portuguese");
+
+    printf("CALCULADORA\n\n");
+    printf("1 - Adição\n");
+    printf("2 - Subtração\n");
+    printf("3 - Multiplicação\n");
+    printf("4 - Divisão\n");
+
+    int opcao;
+
+    printf("\nSelecione a opção para definir a operação: ");\
+    scanf("%d", &opcao);
+
+    if(opcao > 4){
+        system("cls");
+
+        printf("Nenhuma das opções foi selecionada.");
+    }
+
+    float n1, n2, total;
+
+    printf("\nDigite o primeiro número: ");
+    scanf("%f", &n1);
+    printf("\nDigite o segundo número: ");
+    scanf("%f", &n2);
+
+    system("cls");
+
+    switch(opcao){
+        case 1:
+            total = n1 + n2;
+            printf("%.1f + %.1f = %.1f\n", n1, n2, total);
+            break;
+
+        case 2:
+            total = n1 - n2;
+            printf("%.1f - %.1f = %.1f\n", n1, n2, total);
+            break;
+
+        case 3:
+            total = n1 * n2;
+            printf("%.1f * %.1f = %.1f\n", n1, n2, total);
+            break;
+
+        case 4:
+            total = n1 / n2;
+            printf("%.1f / %.1f = %.1f\n", n1, n2, total);
+            break;
+    }
+
+    system("pause");
+    return 0;
+}
