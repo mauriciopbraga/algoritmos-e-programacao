a0300a40742e8308994f197c4ab0a4ef7724aef6
Mauricio P. Braga <mauriciopereirabraga@gmail.com>
2020-05-26 16:39:41 -0300

    aula 17

--- aula-17/exercicio3/exercicio3.c
+++ aula-17/exercicio3/exercicio3.c
@@ -1,1 +1,22 @@
 //Ler dois números e calcular o somatório dos números do intervalo (incluindo, no somatório, os números lidos)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+
+    setlocale(LC_ALL, "portuguese");
+
+    int num1, num2;
+
+    printf("Digite um número inteiro: ");
+    scanf("%d", &num1);
+    printf("Digite outro número inteiro: ");
+    scanf("%d", &num2);
+
+    
+
+    system("pause");
+    return 0;
+}
