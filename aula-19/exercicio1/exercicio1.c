a219959378a7ee0e8a4527ea2d6d2f5337466180
Mauricio P. Braga <mauriciopereirabraga@gmail.com>
2020-06-10 03:30:34 -0300

    aula 19

--- aula-19/exercicio1/exercicio1.c
+++ aula-19/exercicio1/exercicio1.c
@@ -1,1 +1,10 @@
-//Ler vogais e armazenar no vetor apenas vogais que não sejam repetidas.
+//Ler vogais e armazenar no vetor apenas vogais que não sejam repetida.
+
+#include <stdio.h>
+
+int main(){
+
+    
+
+    return 0;
+}
