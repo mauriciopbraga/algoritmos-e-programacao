dcb7f19b43e077893cd5bb9f615c1738083fcdef
Mauricio P. Braga <mauriciopereirabraga@gmail.com>
2020-06-22 15:02:54 -0300

    projetofinal

--- projetofinal/cronometro.c
+++ projetofinal/cronometro.c
@@ -1,18 +1,18 @@
 //Projeto final
-//Timer / Cronômetro
+//Timer / Cronômetro [h:min:s]
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h> //Biblioteca para usar o comando Sleep
 
 int main(){
 
     int horas = 0, minutos = 0, segundos = 0;
 
+    //While setado com a condição 1 significa verdadeiro, ou seja, sempre será executado
     while(1){
         printf("\n[%.2d:%.2d:%.2d]", horas, minutos, segundos);
-        Sleep(1000); //Básicamente o Sleep funciona para pausar os atuais digitos exibidos na tela em milissegundos. No caso, 1000 equivalente a 1 segundo 
-        system("cls");
+        usleep(1000000); //Básicamente o usleep funciona para pausar os atuais digitos exibidos na tela em microssegundos. No caso, 1000000 equivalente a 1 segundo 
+        system("clear");
         segundos++;
 
         if(segundos == 60){
